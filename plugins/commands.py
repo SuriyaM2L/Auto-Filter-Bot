c289b3fa7772ebadee84188416569d259107f3f9
Race Developer Members <88087547+Shivayt007@users.noreply.github.com>
2024-12-05 19:20:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -189,13 +189,8 @@ async def start(client:Client, message):
                 protect_content=settings['file_secure'],
                 reply_markup=InlineKeyboardMarkup(btn)
             )
-            all_files.append(dlt)
-        await asyncio.sleep(600)
-        for dlt_file in all_files:
-            await dlt_file.delete()
-        t=await client.send_message(message.from_user.id, "<b>⚠️ ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛᴇᴅ ꜰɪʟᴇ ɪs ᴅᴇʟᴇᴛᴇᴅ ᴛᴏ ᴀᴠᴏɪᴅ ᴄᴏᴘʏʀɪɢʜᴛ ɪɴ ʙᴏᴛ, ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴀɢᴀɪɴ ᴛʜᴇɴ sᴇᴀʀᴄʜ ᴀɢᴀɪɴ ☺️</b>")
-        await asyncio.sleep(120)
-        await t.delete()
+            
+        
         return
 
     type_, grp_id, file_id = data.split("_", 2)
@@ -243,11 +238,7 @@ async def start(client:Client, message):
         protect_content=settings['file_secure'],
         reply_markup=InlineKeyboardMarkup(btn)
     )
-    await asyncio.sleep(600)
-    await d.delete()
-    r = await message.reply_text("<b>⚠️ ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛᴇᴅ ꜰɪʟᴇ ɪs ᴅᴇʟᴇᴛᴇᴅ ᴛᴏ ᴀᴠᴏɪᴅ ᴄᴏᴘʏʀɪɢʜᴛ, ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴀɢᴀɪɴ ᴛʜᴇɴ sᴇᴀʀᴄʜ ᴀɢᴀɪɴ ☺️</b>")
-    await asyncio.sleep(120)
-    await r.delete()
+    
 
 @Client.on_message(filters.command('settings'))
 async def settings(client, message):
