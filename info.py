5ab1153d860d16ccb884e85a0b3275460416c569
Suriya <76084500+SuriyaM2L@users.noreply.github.com>
2024-12-24 17:45:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '5181226004:AAG7T5qXan-v3X4IIgEaAtdHB6P8uuV
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1491913883 957158815 5011948526').split()]
 USERNAME = environ.get('USERNAME', 'https://telegram.me/Suriya_M2L')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002350694058'))
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001670136532 -1001951551775 -1001813221800 -1001654041347 -1001765415450 -1002070372480').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001898212023 -1001670136532 -1001951551775 -1001813221800 -1001654041347 -1001765415450 -1002070372480').split()]
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://af1:af1@cluster0.cluhr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://af2:af2@cluster0.m6b7l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
