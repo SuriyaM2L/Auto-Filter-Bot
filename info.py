272a71a3052f96af1392d27c24099844f514e851
Suriya <76084500+SuriyaM2L@users.noreply.github.com>
2025-01-18 09:28:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,13 +20,13 @@ def is_enabled(value, default):
 #main variables
 API_ID = int(environ.get('API_ID', '7851526'))
 API_HASH = environ.get('API_HASH', '93ba4db0ad662e558356871afe8ca6de')
-BOT_TOKEN = environ.get('BOT_TOKEN', '1817011647:AAE75TYLPLfQJ6gy97IfiUpjcrdvrXFPWbs')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7935496045:AAEv2kd0fjHqKMPUUbIq6oHEoO27P5zEcYU')
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1491913883').split()]
 USERNAME = environ.get('USERNAME', 'https://telegram.me/Suriya_M2L')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002350694058'))
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001670136532').split()]
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://af1:af1@cluster0.cluhr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://af2:af2@cluster0.m6b7l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002070372480').split()]
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://aftest:aftest@cluster0.rjlq1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002350694058'))
